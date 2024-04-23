51b06a9e14185d818509d1f1e186587b29e0e8be
Olanrewaju Atitebi <63002571+OlanInnovator@users.noreply.github.com>
2024-04-24 00:58:57 +0100

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,15 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t myapp_prod .
+docker tag myapp_prod myapp_prod:latest
 
 # Step 2: 
-# List docker images
+# List docker  images
+docker image ls
 
 # Step 3: 
 # Run flask app
+#docker run -p 8000:5001 myapp_prodsu
+#docker run -it myapp_prodsu bash
+docker run -it --rm --name mydocker myapp_prod
